85867ac09850b5175deab6f26dd2f13b56cd7ac5
yadav767 <yyadavaayush767@gmail.com>
2026-04-24 12:59:11 +0530

    Check plindrome using recursion

---
+++ Day28.js
@@ -0,0 +1,8 @@
+let str = "malayalam";
+function checkPalindrome(str, s, e) {
+    if (s > e) return true
+    if (str[s] != str[e]) return false
+    return checkPalindrome(str, s + 1, e - 1)
+}
+
+console.log(checkPalindrome(str,0,str.length-1));
