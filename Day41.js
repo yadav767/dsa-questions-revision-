7cd8120c8d4209f1c42e90cae1f9132a42b61068
yadav767 <yyadavaayush767@gmail.com>
2026-06-02 23:13:14 +0530

    Binary search

---
+++ Day41.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+function binarySearch(arr, target) {
+    let start = 0;
+    let end = arr.length - 1
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2)
+        if (arr[mid] == target) return mid
+        else if (arr[mid] > target) end = mid - 1
+        else start = mid + 1
+    }
+    return -1
+
+}
+
+console.log(binarySearch(arr,12));
